2846b8626997ebae63cb6cb17fcc7f053c633c1c
Zoe <sfenc101@gmail.com>
2017-10-07 22:53:22 +0800

    Bootstrap success

---
+++ src/BS.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Button from 'react-bootstrap/lib/Button';
+
+class BS extends Component {
+  render() {
+    return (
+      <div>
+            <Button>click me</Button>
+      </div>
+    );
+  }
+}
+
+export default BS;
